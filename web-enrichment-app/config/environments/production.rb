85b323228a64cf28b22431fb81cbcc623a935cd1
Federico Gosman <fedegos@gmail.com>
2026-01-22 01:51:28 +0000

    fix: ajuste de ssl transitorio

--- web-enrichment-app/config/environments/production.rb
+++ web-enrichment-app/config/environments/production.rb
@@ -31,7 +31,9 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
+  # HABILITAR DESPUES
+
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
