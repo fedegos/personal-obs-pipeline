1f92aa979e180b1c0d3a701eb69011677e5c33d7
Federico Gosman <fedegos@gmail.com>
2026-01-22 18:44:23 +0000

    fix: solid cable corregido

--- web-enrichment-app/db/migrate/20260122182039_create_solid_cable_messages.rb
+++ web-enrichment-app/db/migrate/20260122182039_create_solid_cable_messages.rb
@@ -4,8 +4,12 @@ class CreateSolidCableMessages < ActiveRecord::Migration[8.0]
       t.binary :channel, null: false, limit: 1024
       t.binary :payload, null: false, limit: 536870912
       t.datetime :created_at, precision: 6, null: false
+      
+      # Esta es la columna que falta
+      t.integer :channel_hash, limit: 8, null: false 
 
       t.index :channel
+      t.index :channel_hash
       t.index :created_at
     end
   end
